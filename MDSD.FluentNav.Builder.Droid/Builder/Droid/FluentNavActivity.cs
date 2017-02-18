f56923f5c6a7d25e9000e01439f75baae96d4a8b
pgreenbrook <peter.groenbaek@gmail.com>
2017-02-18 14:03:20 +0100

    Simple builder-stuff

--- MDSD.FluentNav.Builder.Droid/Builder/Droid/FluentNavActivity.cs
+++ MDSD.FluentNav.Builder.Droid/Builder/Droid/FluentNavActivity.cs
@@ -1,14 +1,35 @@
+using Android.App;
 using Android.Support.V7.App;
 using MDSD.FluentNav.Builder;
+using MDSD.FluentNav.Metamodel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Android.OS;
 
 namespace MDSD.FluentNav.Builder.Droid
 {
-    public abstract class FluentNavActivity : AppCompatActivity
+    public abstract class FluentNavActivity : AppCompatActivity, INavigation
     {
-        protected abstract void Build();
+        private Navigation Navigation { get; }
+
+        public FluentNavActivity()
+        {
+            Navigation = new Navigation();
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Build(this);
+        }
+
+        protected abstract void Build(INavigation navigation);
+
+        public void View()
+        {
+
+        }
     }
 }
--- MDSD.FluentNav.Example/MDSD.FluentNav.Example.Droid/MainActivity.cs
+++ MDSD.FluentNav.Example/MDSD.FluentNav.Example.Droid/MainActivity.cs
@@ -8,16 +8,19 @@ using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
 using MDSD.FluentNav.Builder.Droid;
+using MDSD.FluentNav.Builder;
 
 namespace MDSD.FluentNav.Example.Droid
 {
-	[Activity (Label = "MDSD.FluentNav.Example", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : FluentNavActivity
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate(bundle);
-		}
-	}
+    [Activity(Label = "MDSD.FluentNav.Example", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FluentNavActivity
+    {
+        protected override void Build(INavigation navigation)
+        {
+            navigation.
+                View()
+            ;
+        }
+    }
 }
 

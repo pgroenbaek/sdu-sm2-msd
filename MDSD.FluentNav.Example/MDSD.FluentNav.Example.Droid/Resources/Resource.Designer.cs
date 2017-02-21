abc51067020553f01c5adcd54d66ee406606aa1a
pgreenbrook <peter.groenbaek@gmail.com>
2017-02-21 15:20:55 +0200

    Added test-views

--- MDSD.FluentNav.Example/MDSD.FluentNav.Example.Droid/Resources/Resource.Designer.cs
+++ MDSD.FluentNav.Example/MDSD.FluentNav.Example.Droid/Resources/Resource.Designer.cs
@@ -59,14 +59,49 @@ namespace MDSD.FluentNav.Example.Droid
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int fragment_green_btn1 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
+		public partial class Layout
+		{
+			
+			// aapt resource value: 0x7f030000
+			public const int fragment_green = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int fragment_yellow = 2130903041;
+			
+			static Layout()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Layout()
+			{
+			}
+		}
+		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f030001
-			public const int ApplicationName = 2130903041;
+			// aapt resource value: 0x7f040001
+			public const int ApplicationName = 2130968577;
 			
-			// aapt resource value: 0x7f030000
-			public const int Hello = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Hello = 2130968576;
 			
 			static String()
 			{
